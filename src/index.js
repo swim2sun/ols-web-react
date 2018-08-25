69a5aae5fb7ad43055c23b4e4caef9ca6b283f0f
alanhg <qianghe421@163.com>
2018-08-25 16:53:57 +0800

    import ant-react

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './assests/css/index.css';
 import Router from './router/index';
 import registerServiceWorker from './registerServiceWorker';
+import 'antd/dist/antd.css';  // or 'antd/dist/antd.less'
 
 ReactDOM.render(<Router />, document.getElementById('root'));
 registerServiceWorker();
